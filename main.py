73c3b48ba0eff773a74c889e1f091abdcf12ca50
officiallyaninja <officiallyaninja@gmail.com>
2022-07-11 00:24:00 +0530

    organized getting card and battle index into 1 function for future refactoring

--- main.py
+++ main.py
@@ -29,39 +29,48 @@ def main():
         current_player_index = (current_player_index + 1) % 2
         print_battles()
         print_current_hand()
-        selected_card = get_input_card()
-        selected_index = get_input_battle_index()
+
+        selected_card, selected_index = get_inputs()
         play_card(selected_card, selected_index)
 
 
 def print_battles():
     for i, skirmish in enumerate(battles):
-        print(f"[{i}] {skirmish}")
+        print(f"[{i + 1}] {skirmish}")
 
 
 def print_current_hand():
     print(players[current_player_index])
 
 
+def get_inputs() -> Tuple[Card, int]:
+    selected_card = get_input_card()
+    # TODO: add undo
+    print(f"you have selected {selected_card}")
+    selected_index = get_input_battle_index()
+    return selected_card, selected_index
+
+
 def get_input_card() -> Card:
     hand: Hand = players[current_player_index]
-    card_index = input_handler.get_int_in_range(1, len(hand) + 1, "enter index of card you want to play: ")
+    card_index = input_handler.get_int_in_range(1, len(hand) + 1, "enter index of card you want to play: ") - 1
 
     return hand.pop(card_index)
 
 
 def get_input_battle_index() -> int:
-    valid_indexes = []
+    valid_inputs = []
     for i, skirmish in enumerate(battles):
         if skirmish.winner is None:
-            valid_indexes.append(i)
+            valid_inputs.append(i + 1)
 
-    index = input_handler.get_int_in_list(valid_indexes, "")
+    index = input_handler.get_int_in_list(valid_inputs, "enter skirmish you would like to play card to") - 1
     return index
 
 
 def play_card(card: Card, battle_index: int) -> None:
     battles[battle_index].add_card(card, current_player_index)
+    players[current_player_index].draw_card(deck)
 
 
 def get_winner() -> Optional[int]:
